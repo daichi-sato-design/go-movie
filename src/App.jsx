4355cf255df89bf0070f0b17c69f7a39b3d92583
daichi-sato-design <daichisato.design@gmail.com>
2021-07-31 17:10:26 +0900

    Routeのクリーンアップ

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Home from "./components/Home";
@@ -8,14 +8,37 @@ import OneGenre from "./components/OneGenre";
 import EditMovie from "./components/EditMovie";
 import Admin from "./components/Admin";
 import Genres from "./components/Genres";
+import Login from "./components/Login";
 
 const App = () => {
+  const [jwt, setJWT] = useState("");
+
+  const logout = () => {
+    setJWT("");
+  };
+
+  let loginLink;
+  if (jwt === "") {
+    loginLink = <Link to="/login">Login</Link>;
+  } else {
+    loginLink = (
+      <Link to="/logout" onClick={logout}>
+        Logout
+      </Link>
+    );
+  }
+
+  const handleJWTChange = () => {};
+
   return (
     <Router>
       <div className="app">
         <div className="container my-4">
           <div className="row">
-            <h1 className="mt-4">映画を見に行こう！</h1>
+            <div className="col mt-3">
+              <h1 className="mt-4">映画を見に行こう！</h1>
+            </div>
+            <div className="col mt-3 text-end">{loginLink}</div>
             <hr className="mb-4" />
           </div>
 
@@ -41,44 +64,42 @@ const App = () => {
                   >
                     ジャンル
                   </Link>
-                  <Link
-                    to="/admin/movie/0"
-                    className="list-group-item list-group-item-action"
-                  >
-                    映画の追加
-                  </Link>
-                  <Link
-                    to="/admin"
-                    className="list-group-item list-group-item-action"
-                  >
-                    カタログ管理
-                  </Link>
+                  {jwt !== "" && (
+                    <>
+                      <Link
+                        to="/admin/movie/0"
+                        className="list-group-item list-group-item-action"
+                      >
+                        映画の追加
+                      </Link>
+                      <Link
+                        to="/admin"
+                        className="list-group-item list-group-item-action"
+                      >
+                        カタログ管理
+                      </Link>
+                    </>
+                  )}
                 </ul>
               </nav>
             </div>
             <div className="col-md-10">
               <Switch>
-                <Route path="/movies/:id">
-                  <OneMovie />
-                </Route>
-                <Route path="/movies">
-                  <Movies />
-                </Route>
-                <Route path="/genre/:id">
-                  <OneGenre />
-                </Route>
-                <Route path="/genres">
-                  <Genres />
-                </Route>
-                <Route path="/admin/movie/:id">
-                  <EditMovie />
-                </Route>
-                <Route path="/admin">
-                  <Admin />
-                </Route>
-                <Route path="/">
-                  <Home />
-                </Route>
+                <Route path="/movies/:id" component={() => <OneMovie />} />
+                <Route path="/movies" component={() => <Movies />} />
+                <Route path="/genre/:id" component={() => <OneGenre />} />
+                <Route path="/genres" component={() => <Genres />} />
+                <Route
+                  path="/admin/movie/:id"
+                  component={() => <EditMovie />}
+                />
+                <Route path="/admin" component={() => <Admin />} />
+                <Route
+                  exact
+                  path="/login"
+                  component={() => <Login handleJWTChange={handleJWTChange} />}
+                />
+                <Route path="/" component={() => <Home />} />
               </Switch>
             </div>
           </div>
